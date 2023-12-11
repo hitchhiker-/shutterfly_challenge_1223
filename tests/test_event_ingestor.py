65efb639268766af8134cc9a4f4af9dafa275074
Bhargava Nissankararao <username@users.noreply.github.com>
2023-12-11 00:28:52 -0500

    added tests

--- tests/test_event_ingestor.py
+++ tests/test_event_ingestor.py
@@ -26,7 +26,44 @@ class TestEventIngestor(unittest.TestCase):
         self.assertIn("96f55c7d8f42", self.data_store["customers"])
         self.assertIsInstance(self.data_store["customers"]["96f55c7d8f42"], Customer)
 
-    # Additional tests for SITE_VISIT, IMAGE, ORDER
+    def test_ingest_site_visit(self):
+        event = {
+            "type": "SITE_VISIT",
+            "verb": "NEW",
+            "key": "ac05e815502f",
+            "event_time": "2017-01-06T12:45:52.041Z",
+            "customer_id": "96f55c7d8f42"
+        }
+        ingest(event, self.data_store)
+        self.assertIn("ac05e815502f", self.data_store["site_visits"])
+        self.assertIsInstance(self.data_store["site_visits"]["ac05e815502f"], SiteVisit)
+
+    def test_ingest_image(self):
+        event = {
+            "type": "IMAGE",
+            "verb": "UPLOAD",
+            "key": "d8ede43b1d9f",
+            "event_time": "2017-01-06T12:47:12.344Z",
+            "customer_id": "96f55c7d8f42",
+            "camera_make": "Canon",
+            "camera_model": "EOS 80D"
+        }
+        ingest(event, self.data_store)
+        self.assertIn("d8ede43b1d9f", self.data_store["images"])
+        self.assertIsInstance(self.data_store["images"]["d8ede43b1d9f"], Image)
+
+    def test_ingest_order(self):
+        event = {
+            "type": "ORDER",
+            "verb": "NEW",
+            "key": "68d84e5d1a88",
+            "event_time": "2017-01-06T12:55:55.555Z",
+            "customer_id": "96f55c7d8f42",
+            "total_amount": "12.34 USD"
+        }
+        ingest(event, self.data_store)
+        self.assertIn("68d84e5d1a88", self.data_store["orders"])
+        self.assertIsInstance(self.data_store["orders"]["68d84e5d1a88"], Order)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_ltv_calculator.py
+++ tests/test_ltv_calculator.py
@@ -1,27 +1,32 @@
 import unittest
 from ltv_calculator import TopXSimpleLTVCustomers
+from events import Customer, SiteVisit, Image, Order
 
 class TestLTVCalculator(unittest.TestCase):
 
     def test_top_x_simple_ltv_customers(self):
         data_store = {
             "customers": {
-                "customer1": Customer(...),  # Mocked Customer instances
-                "customer2": Customer(...)
+                "customer1": Customer("customer1", "2017-01-01T00:00:00.000Z"),  # Mocked Customer instances
+                "customer2": Customer("customer2", "2017-01-01T00:00:00.000Z")
             },
             "site_visits": {
-                # Mocked SiteVisit instances
+                "visit1": SiteVisit("visit1", "2017-01-01T00:00:00.000Z", "customer1"),  # Mocked SiteVisit instances
+                "visit2": SiteVisit("visit2", "2017-01-01T00:00:00.000Z", "customer2")
             },
             "images": {
-                # Mocked Image instances
+                "image1": Image("image1", "2017-01-01T00:00:00.000Z", "customer1"),  # Mocked Image instances
+                "image2": Image("image2", "2017-01-01T00:00:00.000Z", "customer2")
             },
             "orders": {
-                # Mocked Order instances
+                "order1": Order("order1", "2017-01-01T00:00:00.000Z", "customer1", "100.00 USD"),  # Mocked Order instances
+                "order2": Order("order2", "2017-01-01T00:00:00.000Z", "customer2", "200.00 USD")
             }
         }
         result = TopXSimpleLTVCustomers(2, data_store)
         self.assertEqual(len(result), 2)
-        # Additional assertions based on expected LTV values
+        self.assertEqual(result[0].customer_id, "customer2")
+        self.assertEqual(result[1].customer_id, "customer1")
 
 if __name__ == '__main__':
     unittest.main()
